27769eb3bf2c85958ad350efb6fa4c3394cb5ca9
molecula451 <web3samuraidev@gmail.com>
2024-02-29 16:43:43 -0400

    chore: update

--- build/esbuild-build.ts
+++ build/esbuild-build.ts
@@ -1,4 +1,5 @@
 import esbuild from "esbuild";
+import * as dotenv from "dotenv";
 
 const typescriptEntries = ["static/scripts/rewards/index.ts", "static/scripts/audit-report/audit.ts"];
 const cssEntries = ["static/styles/rewards/rewards.css", "static/styles/audit-report/audit.css"];
@@ -18,6 +19,7 @@ export const esBuildContext: esbuild.BuildOptions = {
     ".svg": "dataurl",
   },
   outdir: "static/out",
+  define: createEnvDefines(["SUPABASE_URL", "SUPABASE_ANON_KEY"]),
 };
 
 esbuild
@@ -29,3 +31,17 @@ esbuild
     console.error(err);
     process.exit(1);
   });
+
+function createEnvDefines(envVarNames: string[]): Record<string, string> {
+  const defines: Record<string, string> = {};
+  dotenv.config();
+  for (const name of envVarNames) {
+    const envVar = process.env[name];
+    if (envVar !== undefined) {
+      defines[name] = JSON.stringify(envVar);
+    } else {
+      throw new Error(`Missing environment variable: ${name}`);
+    }
+  }
+  return defines;
+}
